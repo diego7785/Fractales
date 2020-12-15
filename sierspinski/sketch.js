3f618cf370dd6240e1580a21548a537f10bf4b9b
diego7785 <diego.viafara@correounivalle.edu.co>
2020-12-15 13:52:28 -0500

    Triangulo de sierspinski

--- sierspinski/sketch.js
+++ sierspinski/sketch.js
@@ -1,25 +1,24 @@
+var alto;  // Alto del triangulo inicial
 let sf = 1; // factor de escala del zoom
 
 let mx, my; // coordenadas del mouse
 
 function setup() {
+    alto=windowHeight;
     createCanvas(windowWidth, windowHeight);
+    noStroke(); // Elimina los bordes de los triangulos
+    fill(255);
 
     // Slider para cambiar el ángulo de ramificación 
-    slider = createSlider(1, 50, 7,0.1);
+    slider = createSlider(1, 10, 1,1);
     slider.position(windowWidth-100, 10);
     slider.style('width', '80px');
-
-    sliderII = createSlider(2, 200, 7,2);
-    sliderII.position(windowWidth-100, 30);
-    sliderII.style('width', '80px');
 }
 
 function draw() {
     // Posicion del mouse
     mx = mouseX;
     my = mouseY;
-
     background(18,18,36);
 
     translate(mx, my); // Mueve la posicion de creacion de objetos a la posicion dada
@@ -31,29 +30,31 @@ function draw() {
     fill(207, 136, 70);
     triangle(20, 40, 60, 15, 60, 60);
 
-    // Crea la primer rama en el centro de la pantalla
-    translate(width/2, height); 
-    stroke(207, 136, 70);
-    branch(150);
+    fill(255);
+
+    let val = slider.value();
+    
+    let x = width / 2 - alto / 2;
+    let y = height / 2 + alto/2 - 40;
+    
+    divide(x, y, alto, 1, val);
 }
 
-// Crea las ramas del árbol de forma recursiva cambiando el ángulo de creación de las mismas
-function branch(length){
-    line(0,0,0,-length); // Crea la linea
-    translate(0,-length);
-    let valSII = sliderII.value();
-    if(length > valSII){
-        let val = slider.value();
-
-        push(); 
-            rotate(PI/val); // Rota PI/val grados la linea
-            branch(length * 0.75)
-        pop();
-        push();
-            rotate(-PI/val);
-            branch(length * 0.75)
-        pop();
+function divide(x, y, le, lvl, max) {
+    // Creo el primer triángulo 
+    if (lvl == max) {
+      CreateTriangle(x, y, le);
+    } else {
+      // Se crean los triangulos por cada division
+      divide(x, y, le / 2, lvl + 1, max); // Crea el triangulo de abajo a la izquierda
+      divide(x + le / 2, y, le / 2, lvl + 1, max); // Crea el triangulo de abajo a la derecha
+      divide(x + le / 4, y - le * sqrt(3) / 4, le / 2, lvl + 1, max); // Crea el triangulo de arriba
     }
+}
+
+// Crea un triángulo dada la posición (x,y) y su alto (le)
+function CreateTriangle(x, y, le) {
+    triangle(x, y, x + le / 2, y - le * sqrt(3) / 2, x + le, y);
 }
 
 // Función para detectar click en la figura del triangulo que redirige a la página anterior
@@ -66,9 +67,7 @@ function mousePressed() {
 // Implementando zoom al usar la rueda del mouse
 window.addEventListener("wheel", function(e) {
     if (e.deltaY > 0)
-        sf *= 0.95;
+      sf *= 0.95;
     else
-        sf *= 1.05 ;
+      sf *= 1.05 ;
 });
-
-  
